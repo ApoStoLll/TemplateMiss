e5caa164046ffa0ab8e36ed516e327e015efd114
neket <neketapostolov@gmail.com>
2021-04-16 18:22:47 +0300

    first commit

---
+++ app/src/main/java/com/missclick/missappstemplate/App.java
@@ -0,0 +1,27 @@
+package com.missclick.missappstemplate;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+public class App extends Application {
+
+    static final String AF_DEV_KEY = "2KTn2FFy4YwGYoQEgmsDCg";
+    static final String BASE_URL = "https://herlionse.space/2ZDW2p";
+    static final String ORGANIC_URL = "https://herlionse.space/2ZDW2p";
+    static final String URL_REDIRECT = "github.com";
+    static final String PACKAGE = "com.missclick.missappstemplate";
+    private static final String ONESIGNAL_APP_ID = "d878f387-dc9b-48b8-9e30-86acb0c1d496";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+    }
+}
